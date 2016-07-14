350b864692bf5aab6149feaf62a2682aa7c1de70
caghan <caghan@gmail.com>
2016-07-14 23:57:27 +0300

    Fixed nanosleep

--- tsudpstreamer.go
+++ tsudpstreamer.go
@@ -1,43 +1,58 @@
 package main
 
 import (
+	"encoding/hex"
 	"io"
 	"log"
 	"net"
 	"os"
+	"syscall"
 	"github.com/aristanetworks/goarista/atime" //for getting monotonic clock time
 )
 
 const (
-	MULTICAST_ADDR = "127.0.0.1:12345"
-	TS_FILE_NAME = "test.ts"
-	MPEG_TS_BITRATE = 9358043
-	TS_PACKET_SIZE = 188
-	TS_BUFFER_SIZE = TS_PACKET_SIZE * 8 // 1316 for UDP streaming
-	//ONE_TS_PACKET_NS_FOR_100MPS = 665778 // 1 packet at 100mbps
-	LOOP_TS = true
+	srvAddr = "224.0.0.1:12345"
+	maxDatagramSize = 1316
+	fName           = "test.ts"
+	bitrate         = 1030949
+	TSPacketSize    = 188
+	fileChunkSize   = 500
 )
 
-func checkError(e error) {
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
 func main() {
-	log.Println("Streaming of ", TS_FILE_NAME, "started!")
-	udpStreamTo(MULTICAST_ADDR)
-	log.Println("Streaming of ", TS_FILE_NAME, "ended!")
+	/*t1 := atime.NanoTime()
+	t2 := atime.NanoTime()
+	if t1 >= t2 {
+		log.Fatalln("t1 should have been strictly less than t2 ", t1, t2)
+	} else {
+		log.Println("t1 is strictly less than t2 ", t1, t2)
+	}
+	*/
+	ping(srvAddr)
+	//serveMulticastUDP(srvAddr, msgHandler)
 }
 
-func udpStreamTo(multicastAddr string) {
-	addr, err := net.ResolveUDPAddr("udp", multicastAddr)
-	checkError(err)
-	udpConn, err := net.DialUDP("udp", nil, addr)
-	checkError(err)
-	tsFile, err := os.Open(TS_FILE_NAME)
-	checkError(err)
-	tsBuffer := make([]byte, TS_BUFFER_SIZE)
+func ping(a string) {
+	addr, err := net.ResolveUDPAddr("udp", a)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	packetSize := 7 * TSPacketSize
+
+	c, err := net.DialUDP("udp", nil, addr)
+	file, err := os.Open(fName)
+	check(err)
+	defer file.Close()
+
+	buf := make([]byte, packetSize)
+	//reader := bufio.NewReader(file)
 
 	completed := 0
 	packetTime := uint64(0)
@@ -45,35 +60,67 @@ func udpStreamTo(multicastAddr string) {
 	timeStop := uint64(0)
 	realTime := uint64(0)
 
+	nanoSleepPacket := syscall.Timespec{}
+	nanoSleepPacket.Nsec = 665778 // 1 packet at 100mbps
+
 	timeStart = atime.NanoTime()
 
+	c.SetWriteBuffer(229376)
 	for completed != 1 {
+
 		timeStop = atime.NanoTime()
-		realTime = (timeStop - timeStart) / 1000
-		if realTime* MPEG_TS_BITRATE > packetTime*1000000 && completed != 1 {
-			chunk, err := tsFile.Read(tsBuffer)
+		realTime = (timeStop - timeStart)
+
+		if realTime*bitrate > packetTime*1000000 && completed != 1 {
+
+			tmp, err := file.Read(buf)
+			//check(err)
 			if err != nil {
 				if err == io.EOF {
 					completed = 1
-					if LOOP_TS {
-						udpStreamTo(multicastAddr)
-					}
+					log.Println("HEDEEE")
 				} else {
 					panic(err)
 				}
 			}
-			if chunk < 0 {
-				log.Println("TS FILE SENT!")
+			if tmp < 0 {
+				log.Println("ts sent done.")
 				completed = 1
-			} else if chunk == 0 {
-				log.Println("TS FILE READ ERROR!")
+			} else if tmp == 0 {
 				completed = 1
 			} else {
-				udpConn.Write(tsBuffer[:chunk])
-				packetTime += uint64(TS_BUFFER_SIZE * 8)
+				log.Println("bytes: ", tmp, ", string: ", hex.Dump(buf[:tmp]))
+				c.Write(buf[:tmp])
+				packetTime += uint64(packetSize * 8)
+
 			}
 		} else {
-			//syscall.Nanosleep(&ONE_TS_PACKET_NS_FOR_100MPS, nil) // DOESN'T WORK ON MAC OS X!
+			log.Println("syscall nanosleep!!!!!!!!!!!!!!!!! ")
+			syscall.Nanosleep(&nanoSleepPacket, nil)
 		}
+
+		//time.Sleep(1 * time.Second)
+	}
+}
+
+func msgHandler(src *net.UDPAddr, n int, b []byte) {
+	log.Println(n, "bytes read from", src)
+	log.Println(hex.Dump(b[:n]))
+}
+
+func serveMulticastUDP(a string, h func(*net.UDPAddr, int, []byte)) {
+	addr, err := net.ResolveUDPAddr("udp", a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	l, err := net.ListenMulticastUDP("udp", nil, addr)
+	l.SetReadBuffer(maxDatagramSize)
+	for {
+		b := make([]byte, maxDatagramSize)
+		n, src, err := l.ReadFromUDP(b)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed:", err)
+		}
+		h(src, n, b)
 	}
 }
